e156fa884291d61cfa1d728e3f7cc1f0f2f4cd29
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-04-16 15:24:40 -0700

    Updating city model

--- models/city.py
+++ models/city.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    if storage_type == "db":
+        __tablename__ = "cities"
+
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all, delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes City"""
+        super().__init__(*args, **kwargs)
