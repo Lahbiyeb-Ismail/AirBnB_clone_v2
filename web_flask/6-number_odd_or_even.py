60ed507c27f18c18912317c8b1c6167ea899c684
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-17 13:38:08 +0100

    Update 5-number.html template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 script that starts a Flask web application
 """
@@ -9,19 +11,35 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
-    return "Hello, HBNB!"
+    """
+    This function returns a greeting message.
+    """
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    Returns the string "HBNB".
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
+    """
+    Replaces underscores in the given text with spaces
+    and prefixes the text with 'C'.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The modified text with underscores replaced
+        by spaces and prefixed with 'C'.
+    """
     new_text = [" "] * len(text)
 
-    # new_text = text.replace("_", " ")
     for i in range(len(text)):
         if text[i] != "_":
             new_text[i] = text[i]
@@ -36,23 +54,64 @@ def c_route(text):
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_route(text):
-    new_text = text.replace("_", " ")
+    """
+    Replaces underscores with spaces in the given text
+    and returns a formatted string.
 
+    Args:
+        text (str): The input text to be processed.
+
+    Returns:
+        str: A formatted string with "Python" followed
+        by the modified text.
+
+    Example:
+        >>> python_route("hello_world")
+        'Python hello world'
+    """
+    new_text = text.replace("_", " ")
     return f"Python {new_text}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
+    """
+    Returns a string representation of the given number.
+
+    Parameters:
+    n (int): The number to be converted to a string.
+
+    Returns:
+    str: A string representation of the given number.
+    """
     return f"{n} is a number"
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
+    """
+    Renders a template with the given number.
+
+    Args:
+        n (int): The number to be displayed in the template.
+
+    Returns:
+        The rendered template with the number.
+    """
     return render_template("5-number.html", number=n)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
+    """
+    Determine if a number is odd or even.
+
+    Args:
+        n (int): The number to be checked.
+
+    Returns:
+        str: The type of the number, either "even" or "odd".
+    """
     number_type = ""
     if n % 2 == 0:
         number_type = "even"
