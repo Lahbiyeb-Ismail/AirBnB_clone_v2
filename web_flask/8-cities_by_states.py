f727d35afc8b6fa565bffe87aca370989a38757a
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-17 15:53:33 +0100

    chore: Update Flask web application script and template for cities by states page

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,8 +10,8 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """
     Retrieve a list of states and render them in a template.
 
@@ -19,9 +19,8 @@ def states_list():
         The rendered template with the list of states.
     """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    print(states)
 
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
