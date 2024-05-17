e6bab1b8d72321e3057f4b343006bb97d4e75b75
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-17 18:29:42 +0100

    Update states_list template to use storage.all() and sort states by name

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application
+starts a Flask web application
 """
 
 from flask import Flask, render_template
 
+from models import *
 from models import storage
 
 app = Flask(__name__)
@@ -12,14 +13,8 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """
-    Retrieve a list of states and render them in a template.
-
-    Returns:
-        The rendered template with the list of states.
-    """
-    states = storage.all()
-
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
@@ -30,4 +25,4 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
