061a93185b69de2340b72f4aecde38b4b403a950
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-17 13:29:40 +0100

    add docs string to python_route function

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 script that starts a Flask web application
 """
@@ -9,19 +11,35 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
-    return "Hello, HBNB!"
+    """
+    This function returns a greeting message.
+    """
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    Returns the string "HBNB".
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
+    """
+    Replaces underscores in the given text with spaces
+    and prefixes the text with 'C'.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The modified text with underscores replaced
+        by spaces and prefixed with 'C'.
+    """
     new_text = [" "] * len(text)
 
-    # new_text = text.replace("_", " ")
     for i in range(len(text)):
         if text[i] != "_":
             new_text[i] = text[i]
@@ -36,8 +54,22 @@ def c_route(text):
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_route(text):
-    new_text = text.replace("_", " ")
+    """
+    Replaces underscores with spaces in the given text
+    and returns a formatted string.
 
+    Args:
+        text (str): The input text to be processed.
+
+    Returns:
+        str: A formatted string with "Python" followed
+        by the modified text.
+
+    Example:
+        >>> python_route("hello_world")
+        'Python hello world'
+    """
+    new_text = text.replace("_", " ")
     return f"Python {new_text}"
 
 
